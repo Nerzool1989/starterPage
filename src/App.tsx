93b3605603374382612b18ad8a473026c3ddad9d
Nerzool1989 <nerzool1989@yandex.ru>
2022-10-31 16:15:12 +0300

    step2 webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import './style.css';
+import reactlogo from './reactlogo.jpg';
+import reactsvglogo from './react.svg';
 
 export const App = () => {
-    return <div>Hello</div>
+    return (
+        <div>
+            <h1>Hello</h1>
+            <img src={reactlogo} alt="react logo" width='300' height='300'/>
+            <img src={reactsvglogo} alt="react logo" width='300'/>
+        </div>
+    )
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,8 +15,20 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                     },
-                ]
-
+                ],
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/inline'
             }
         ]
     },
